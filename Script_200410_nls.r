5335b30e85a88a2a248dfae06f900894ded377cb
pgiraudoux <patrick.giraudoux@univ-fcomte.fr>
2020-04-17 15:30:05 +0200

    update

--- Script_200410_nls.r
+++ Script_200410_nls.r
@@ -15,10 +15,14 @@ Chad<-mescas
 ex<-list(Chad=Chad,Algeria=Algeria,Uganda=Uganda)
 
 save(ex,file="Examples.Rdata")
-pays<-ecdc[ecdc$countriesAndTerritories=="Italy",]
+
+
+pays<-ecdc[ecdc$countriesAndTerritories=="Namibia",]
 pays<-pays[order(pays$dateRep),]
+pays
 firstcase<-which(cumsum(pays$cases)>=start)[1]
 plot(pays$dateRep[firstcase:length(pays$cases)],cumsum(pays$cases)[firstcase:length(pays$cases)],las=1,type="l",las=1,xlab="",ylab="cases")
+
 
 cumsum(pays$deaths)
 
